1a2e51cff3d8ff0095bbb87022b802613b8f375f
ackno <56048078+ackno@users.noreply.github.com>
2019-10-13 12:53:49 +0200

    change struct

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 30px;
+  background-color: yellow;
+  border: 3px solid black;
+`;
+
+export default Button;
---
+++ src/views/Root/Root.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <>
+    <Button>Hello</Button>
+  </>
+);
+
+export default Root;
