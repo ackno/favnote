43df8910f04e34dbf458a29ef8da599074b4c55a
ackno <56048078+ackno@users.noreply.github.com>
2019-10-13 13:43:30 +0200

    add GlobalStyle

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
   <>
+    <GlobalStyle />
     <Button>Hello</Button>
     <Button secondary>world</Button>
     <Button width="400px">Och</Button>
