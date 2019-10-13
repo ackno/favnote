6afd637feeaa7994428932dd5130e735f28b5437
ackno <56048078+ackno@users.noreply.github.com>
2019-10-13 14:51:31 +0200

    add withKnobs to Button.stories

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, select, number } from '@storybook/addon-knobs';
 
 import Button from './Button';
 
-storiesOf('Button', module)
-  .add('Primary', () => <Button>Hello world</Button>)
-  .add('Secondary', () => <Button secondary>Hello world</Button>)
-  .add('with width', () => <Button width="600px">Hello world</Button>);
+const stories = storiesOf('Button', module);
+stories.addDecorator(withKnobs);
+
+stories.add('as primary with dynamic props', () => {
+  const group = 'GROUP-ID-1';
+
+  const colorOptions = {
+    yellow: 'hsl(49, 100%, 58%)',
+    blue: 'hsl(196, 83%, 75%)',
+    green: 'hsl(106, 47%, 64%)',
+  };
+  const defaultValue = colorOptions.yellow;
+  const color = select('Background color', colorOptions, defaultValue, group);
+
+  const widthOptions = {
+    range: true,
+    min: 50,
+    max: 800,
+    step: 10,
+  };
+  const width = number('Width (px)', '220', widthOptions, group);
+
+  return (
+    <Button color={color} width={`${width}px`}>
+      Hello World
+    </Button>
+  );
+});
+
+stories.add('as secondary', () => <Button secondary>Hello World</Button>);
